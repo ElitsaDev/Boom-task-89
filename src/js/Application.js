4eb0f90f571a7b424a44a5641c43aaa2562e23dc
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-01-29 14:34:01 +0200

    fetch then and await logic

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,38 +10,76 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
+    this._loading = document.querySelector(".progress");
+    this.url = `https://swapi.boom.dev/api/planets`;
 
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.innerHTML = this._render({
-      name: "Placeholder",
-      terrain: "placeholder",
-      population: 0,
-    });
-
-    document.body.querySelector(".main").appendChild(box);
+    this._startLoading();
+    this._create();
 
     this.emit(Application.events.READY);
   }
 
+  _startLoading() {
+    this._loading.style.display = "block";
+  }
+
+  _create() {
+    this._load()
+    .then((response) => {
+      response.results.forEach((element) => {
+          const box = document.createElement("div");
+          box.classList.add("box");
+          box.innerHTML = this._render({
+            name: element.name,
+            terrain: element.terrain,
+            population: element.population,
+          });
+           this._stopLoading();
+           document.body.querySelector(".main").appendChild(box);
+      });
+      return response.results;
+    })
+    .then((response) => {
+        if(response.next){
+            this.url = response.next;
+            this._create;
+        }  
+    }).catch(err => {
+      console.log(`Can't create...Error message: ${err}`);
+    });
+  }
+
+  _load = async function _load() {
+    return await fetch(this.url)
+      .then((response) => {
+        return response.json();
+      }).catch(err => {
+        console.log(`Can't load...Error message: ${err}`);
+      });
+  }
+
+  _stopLoading() {
+      this._loading.style.display = "none";
+  }
+
   _render({ name, terrain, population }) {
     return `
-<article class="media">
-  <div class="media-left">
-    <figure class="image is-64x64">
-      <img src="${image}" alt="planet">
-    </figure>
-  </div>
-  <div class="media-content">
-    <div class="content">
-    <h4>${name}</h4>
-      <p>
-        <span class="tag">${terrain}</span> <span class="tag">${population}</span>
-        <br>
-      </p>
-    </div>
-  </div>
-</article>
-    `;
+    <article class="media">
+      <div class="media-left">
+        <figure class="image is-64x64">
+          <img src="${image}" alt="planet">
+        </figure>
+      </div>
+      <div class="media-content">
+        <div class="content">
+        <h4>${name}</h4>
+          <p>
+            <span class="tag">${terrain}</span> <span class="tag">${population}</span>
+            <br>
+          </p>
+        </div>
+      </div>
+    </article>
+        `;
   }
 }
