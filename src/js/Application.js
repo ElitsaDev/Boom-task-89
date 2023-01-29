2ec90ac7e48aad35ab5bb47fcb39512ded5eb514
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-01-29 19:29:11 +0200

    refactoring of application.js

--- src/js/Application.js
+++ src/js/Application.js
@@ -2,68 +2,78 @@ import EventEmitter from "eventemitter3";
 import image from "../images/planet.svg";
 
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+    static get events() {
+        return {
+            READY: "ready",
+        };
+    }
 
-  constructor() {
-    super();
-    this._loading = document.querySelector(".progress");
-    this.url = `https://swapi.boom.dev/api/planets`;
+    constructor() {
+        super();
+        this._loading = document.querySelector(".progress");
+        this.url = `https://swapi.boom.dev/api/planets`;
 
-    this._startLoading();
-    this._create();
+        this._startLoading();
+        this._create();
 
-    this.emit(Application.events.READY);
-  }
+        this.emit(Application.events.READY);
+    }
 
-  _startLoading() {
-    this._loading.style.display = "block";
-  }
+    _startLoading() {
+        this._loading.style.display = "block";
+    }
 
-  _create() {
-    this._load()
-    .then((response) => {
-      response.results.forEach((element) => {
-          const box = document.createElement("div");
-          box.classList.add("box");
-          box.innerHTML = this._render({
-            name: element.name,
-            terrain: element.terrain,
-            population: element.population,
-          });
-           this._stopLoading();
-           document.body.querySelector(".main").appendChild(box);
-      });
-      return response.results;
-    })
-    .then((response) => {
-        if(response.next){
-            this.url = response.next;
-            this._create;
-        }  
-    }).catch(err => {
-      console.log(`Can't create...Error message: ${err}`);
-    });
-  }
+    _create() {
+        this._load()
+            .then((response) => {
 
-  _load = async function _load() {
-    return await fetch(this.url)
-      .then((response) => {
-        return response.json();
-      }).catch(err => {
-        console.log(`Can't load...Error message: ${err}`);
-      });
-  }
+                response.results.forEach((planet) => {
+                    const box = document.createElement("div");
+                    box.classList.add("box");
+                    box.innerHTML = this._render({
+                        name: planet.name,
+                        terrain: planet.terrain,
+                        population: planet.population,
+                    });
+                    document.body.querySelector(".main").appendChild(box);
+                });
 
-  _stopLoading() {
-      this._loading.style.display = "none";
-  }
+                return response.results;
 
-  _render({ name, terrain, population }) {
-    return `
+            })
+            .then((response) => {
+
+                if (response.next) {
+                    this.url = response.next;
+                    this._create;
+                } else {
+                    this._stopLoading();
+                }
+
+            }).catch(err => {
+                console.log(`Can't create...Error message: ${err}`);
+            });
+    }
+
+    async _load() {
+
+        const response = await fetch(this.url);
+
+        if (!response.ok) {
+            console.log(`Can't load...Error status: ${response.status}`);
+            return;
+        }
+
+        const planets = await response.json();
+        return planets;
+    }
+
+    _stopLoading() {
+        this._loading.style.display = "none";
+    }
+
+    _render({ name, terrain, population }) {
+        return `
     <article class="media">
       <div class="media-left">
         <figure class="image is-64x64">
@@ -81,5 +91,5 @@ export default class Application extends EventEmitter {
       </div>
     </article>
         `;
-  }
+    }
 }
